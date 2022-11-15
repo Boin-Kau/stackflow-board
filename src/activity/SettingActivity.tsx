43e0e9f5758915af2590b57e95af49335097c365
Boin Kim <charles1236@kau.kr>
2022-11-15 15:45:03 +0900

    :sparkles: feat: Activity Stack monitoring

--- src/activity/SettingActivity.tsx
+++ src/activity/SettingActivity.tsx
@@ -1,12 +1,25 @@
-import { ActivityComponentType } from "@stackflow/react";
+import { ActivityComponentType, useStack } from "@stackflow/react";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
+import { useEffect } from "react";
 
 const SettingActivity: ActivityComponentType = () => {
+  const stack = useStack();
+
+  useEffect(() => {
+    console.log("현재 쌓여진 액티비티들:", stack.activities);
+    console.log("전체 전환 상태:", stack.globalTransitionState);
+    console.log(
+      "초기에 설정된 Transition Duration 옵션",
+      stack.transitionDuration
+    );
+  }, [stack]);
+
   return (
     <AppScreen
       appBar={{
         title: "설정",
       }}
+
     >
       <div>Setting Activity</div>
     </AppScreen>
